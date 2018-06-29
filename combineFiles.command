f89c5d7d91914e02342d03cbdeae25b831750c58
Benjamin Lawrence <benjaminlawrence@Benjamins-MacBook-Pro.local>
2018-06-29 10:46:25 -0400

    made combineFiles a .command file so it could be executed and finished program but hasn't been debugged yet

---
+++ combineFiles.command
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# find current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+# if homebrew is not installed, install homebrew
+if [[ $(command -v brew) == "" ]]; then
+    echo "Installing Homebrew\n"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+	brew upgrade
+fi
+
+# if imagemagick is not installed, install imagemagick
+if ! type "$convert" > /dev/null; then
+    echo "Installing imagemagick"
+    brew install imagemagick
+fi
+
+# combining JPG files into one PDF
+echo "Starting combination of JPGs into one file"
+convert *.jpg FinishedPDFDocument.pdf
+echo "Finished combining JPGs into one PDF"
+echo "Starting deletion of JPGs"
+rm *.jpg
+echo "Finished deleting JPGs"
+
+# combining any desired PDF files into one PDF
+echo "Would you like to combine any PDF files? (Type yes or no and press enter)"
+read response
+
+if [ "$response" == "yes" ]; then
+	# if poppler is not installed, then install it
+	if ! type "$pdfunite" > /dev/null; then
+	    echo "Installing poppler"
+	    brew install poppler
+	fi
+
+	echo "Please make sure that your PDF files are named 1.pdf, 2.pdf, 3.pdf, ... according to the order in which you would like them to be combined."
+	read -p "Press enter when you are ready. (Press Ctrl+C if you accidentally typed yes and want to quit)"
+
+	echo "Starting combination of PDF files."
+	pdfunite $(ls -v *.pdf) output.pdf
+	echo "Finished combining PDF files."
+fi
+
+echo ""
+echo "Thank you for using our program and please click the terminate button come back soon :)"
+
+# quit terminal window
+osascript -e 'quit app "terminal.app"'
--- combineFilesUpdated.sh
+++ combineFilesUpdated.sh
@@ -6,13 +6,13 @@ cd "$DIR"
 
 # if homebrew is not installed, install homebrew
 if [[ $(command -v brew) == "" ]]; then
-    echo "Installing Hombrew"
+    echo "Installing Homebrew\n"
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # if imagemagick is not installed, install imagemagick
-if [[ $(command -v convert) == "" ]]; then
-    echo "Installing imagemagick"
+if [[ $(command -h convert) == "" ]]; then
+    echo "Installing imagemagick\n"
     brew install imagemagick
 fi
 
@@ -23,23 +23,27 @@ echo "Finished combining JPGs into one PDF\n"
 echo "Starting deleting of JPGs\n"
 rm *.jpg
 echo "Finished deleting JPGs\n"
-echo "What would you like to call the new PDF file? (Press enter when you have typed the name)"
-read PDFName
-mv FinishedPDFDocument.pdf ${PDFName}.pdf
 
 # combining any desired PDF files into one PDF
 echo "\nWould you like to combine any PDF files? (Type yes or no and press enter)\n"
 read response
 
-while [ "$response" != "no" ]; do
-	echo "Please type the name of the first PDF document you would like to add. Type no if you are done"
-	read response
-	
-done
-	
+if [ "$response" == "yes" ]; then
+	# if pdfunite is not installed, then install it
+	if [[ $(command -h pdfunite) == "" ]]; then
+	    echo "Installing pdfunite\n"
+	    brew install pdfunite
+	fi
 
-echo "\nBeginning combination of PDF files into one file\n"
-echo "Finished combining PDFs into one file\n"
+	echo "Please make sure that your PDF files are named 1.pdf, 2.pdf, 3.pdf, ... according to the order in which you would like them to be combined.\n"
+	read -p "Press enter when you are ready. (Press Ctrl+C if you accidentally typed yes and want to quit)\n"
+
+	echo "Starting combination of PDF files.\n"
+	pdfunite $(ls -v *.pdf) output.pdf
+	echo "Finished combining PDF files.\n"
+fi
+
 echo "\nThank you for using our program and please come back soon :)\n"
+
 # quit terminal window
 osascript -e 'quit app "terminal.app"'
